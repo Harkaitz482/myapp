c7da6a6075feb579dbecbf7e3f8b4009f98e3c19
Harkaitz482 <harkatrujillo@hotmail.com>
2024-01-15 21:39:26 +0000

    favicon

--- app.js
+++ app.js
@@ -9,10 +9,17 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+app.listen(3001)
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+var favicon = require('serve-favicon')
 
+var app = express()
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+// Add your routes here, etc.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -23,12 +30,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
